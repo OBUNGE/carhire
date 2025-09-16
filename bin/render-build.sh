ae3362dc618af28ef970dda1aaeeacf1581c4352
FELIX OMONDI ODHIAMBO <felixokiya10@gmail.com>
2025-09-16 14:35:42 +0300

    Fix Render build by installing Yarn before asset build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# --- Ensure Yarn is available ---
+# Corepack comes with Node.js >= 16 and manages Yarn versions
+corepack enable
+corepack prepare yarn@stable --activate
+
+# --- Install Ruby gems ---
 bundle install
+
+# --- Install JS dependencies ---
+yarn install --check-files
+
+# --- Build assets ---
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+# --- Run database migrations ---
 bundle exec rake db:migrate
-
-
